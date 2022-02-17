8d3083dfc55d97790545f03fe99cb833646cbd53
user <user@gmail.com>
2022-02-17 19:56:30 +0500

    jjj

---
+++ app/src/main/java/com/example/myapplication/QRMainActivity2.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
+
+class QRMainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrmain2)
+
+    }
+}
